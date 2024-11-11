fbe71bd17a8a10333c485aebb3e7b15cb371a96d
ttchabs <tchabs334@gmail.com>
2024-11-11 09:37:31 +0200

    baking

--- Assets/Scripts/HP/HealthGain.cs
+++ Assets/Scripts/HP/HealthGain.cs
@@ -22,6 +22,7 @@ public class HealthGain : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             InventoryManager.Instance.AddConsumable(HealthGrubsData);
+            Destroy(gameObject);
         }
     }
 }
